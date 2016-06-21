6d2bce99a0646f20b3942953a84adf46710bf18f
sunsplat <heeycutie@yahoo.com>
2016-06-21 11:32:36 -0400

    first commit

---
+++ battleship.js
@@ -0,0 +1,12 @@
+var location1;
+var location2;
+var location3;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+if (hits == 3) {
+	isSunk = true;
+	alert('You sank my battleship!');
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Play Battleship!
+===================================
+Can you find my battleship? Try your luck [here](http://sunsplat.github.io/battleship/index.html).  
